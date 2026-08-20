43e78e38a01d5e90883aea413b56867281d68d02
olegchertkov1974-lang <olegchertkov1974@gmail.com>
2026-08-20 15:37:41 +0300

    Запускать упаковку Netlify через bash

--- scripts/build-platform.sh
+++ scripts/build-platform.sh
@@ -5,7 +5,7 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if [[ "${NETLIFY:-}" == "true" ]]; then
   "${script_dir}/../node_modules/.bin/next" build
-  exec "${script_dir}/package-netlify.sh"
+  exec bash "${script_dir}/package-netlify.sh"
 fi
 
 exec "${script_dir}/build-verified.sh"
